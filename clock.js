1c3e2de97fe999e1e4440bb3111cd86fc6a070eb
Alwasib <sheikhminan5@gmail.com>
2025-03-02 19:38:16 +0600

    added new

--- clock.js
+++ clock.js
@@ -1,6 +1,14 @@
 document.getElementById("clock").textContent = new Date().toDateString().split(' ')[0]+" ,";
 document.getElementById("clock2").textContent = new Date().toDateString().slice(4);
 
+document.getElementById("time").textContent = new Date().toLocaleTimeString();
+document.getElementById("time2").textContent = new Date().toLocaleTimeString();
+document.getElementById("time3").textContent = new Date().toLocaleTimeString();
+document.getElementById("time4").textContent = new Date().toLocaleTimeString();
+document.getElementById("time5").textContent = new Date().toLocaleTimeString();
+document.getElementById("time6").textContent = new Date().toLocaleTimeString();
+
+
 
 
 
