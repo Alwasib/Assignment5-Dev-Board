ba60fb9fc5d0a45acb9a24bc0fd0a3cf48cd19b2
Alwasib <sheikhminan5@gmail.com>
2025-03-02 11:06:22 +0600

    added new

---
+++ clock.js
@@ -0,0 +1,6 @@
+document.getElementById("clock").textContent = new Date().toDateString().split(' ')[0]+" ,";
+document.getElementById("clock2").textContent = new Date().toDateString().slice(4);
+
+
+
+
